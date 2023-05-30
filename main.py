1022df8ef9c9bfd94de14b1046d9f93b21f8d155
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-05-30 15:46:05 +0200

    Add player and change background

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import pygame
 
-#Initialize the pygame
+# Initialize the pygame
 pygame.init()
 
-#Create the screen
+# Create the screen
 screen = pygame.display.set_mode((800, 600))
 
-#Game Loop
+# Title and Icon
+pygame.display.set_caption("Space Invaders by Lesiuu")
+icon = pygame.image.load('rocket.png')
+pygame.display.set_icon(icon)
+
+
+# Player
+playerImg = pygame.image.load('starfighter.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg,(playerX,playerY))
+
+
+# Game Loop
 running = True
+
+
 while running:
+
+    screen.fill((0, 0, 200))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
