e23c304f295c15650517fa10c44cb5b69da04357
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-05-30 16:47:54 +0200

    Add enemy movement

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ playerX_change = 0
 enemyImg = pygame.image.load('ufo.png')
 enemyX = random.randint(0,800)
 enemyY = random.randint(50,150)
-enemyX_change = 0
+enemyX_change = 0.3
+enemyY_change = 40
 
 
 
@@ -56,12 +57,23 @@ while running:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
-
+    # Checking for boundaries
     playerX += playerX_change
+
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+    # Enemy movement
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
     player(playerX,playerY)
     enemy(enemyX,enemyY)
     pygame.display.update()
